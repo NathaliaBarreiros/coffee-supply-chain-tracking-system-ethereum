98fd3ec7ba53dd07a04edef7bca26a0a94b49844
Nath <nathalia.barreirosf@gmail.com>
2022-03-06 19:14:11 -0500

    SupplyChainStorage testing done

--- test/SupplyChainStorage.js
+++ test/SupplyChainStorage.js
@@ -1,7 +1,6 @@
 const SupplyChainStorage = artifacts.require('SupplyChainStorage');
-// const assert = require('chai').assert;
 
-const _name = 'Ram';
+const _name = 'Mat';
 const _contactNo = '0979081091';
 const _role = 'FARMER';
 const _isActive = true;
@@ -14,6 +13,7 @@ contract('SupplyChainStorage', function(accounts){
 
     beforeEach(async() => {
         this.storageContract = await SupplyChainStorage.new({from:spenderAddress, gas: 6000000});
+        // await this.storageContract.authorizeCaller(this.storageContract.address,{from:spenderAddress});
     });
 
     it('should Authorize', async() => {
@@ -32,43 +32,28 @@ contract('SupplyChainStorage', function(accounts){
         // console.log(deAuthorizeCallerEvent);
     });
 
-    it('should Add/Update New User', async() => {
-        const {logs} = await this.storageContract.setUser(userAddress,_name, _contactNo, _role, _isActive, _profileHash, {from: spenderAddress});
 
-        console.log(typeof(logs));
-        checkUserExists(logs, function(result){
-            console.log(result);
-        });
+    it('should Add New User', async() => {
 
-        const user = await this.storageContract.getUser(userAddress, {from: spenderAddress});
-        // console.log("Here")
-        // console.log(typeof(user));
-        // console.log(user[0]);
-        console.log(user[0]);
+        const {logs} = await this.storageContract.setUser(userAddress,_name, _contactNo, _role, _isActive, _profileHash)
+            //, {from: spenderAddress});
 
-        // checkUserData(user, function(result){
-        //     console.log("checkUserData");
-        // });
+        const updateUserEvent = logs.find(e => e.event === 'UserUpdate');
+        assert.exists(updateUserEvent, "UserUpdate does not exists");
+    
+        const updateUserRoleEvent = logs.find(e => e.event === 'UserRoleUpdate');
+        assert.exists(updateUserRoleEvent, "UserRoleUpdate does not exists");
+
+        const user = await this.storageContract.getUser.call(userAddress);
+        // console.log("HERE")
+        // console.log(user);
+
+        //expect(_contactNo).to.equal(user[1]);
+        assert.equal(user[0],_name,"Name checked:");
+        assert.equal(user[1],_contactNo,"Contact No checked:");
+        assert.equal(user[2],_role,"Role checked:");
+        assert.equal(user[3],_isActive,"isActive checked:");
+        assert.equal(user[4],_profileHash,"Profile Hash checked:");
     });
 })
 
-function checkUserExists(logs, callback) {
-    const updateUserEvent = logs.find(e => e.event === 'UserUpdate');
-    assert.exists(updateUserEvent, "UserUpdate does not exists");
-
-    const updateUserRoleEvent = logs.find(e => e.event === 'UserRoleUpdate');
-    assert.exists(updateUserRoleEvent, "UserRoleUpdate does not exists");
-
-    callback(true);
-}
-
-function checkUserData(user, callback){
-    assert.equal(user[0],_name,"Name checked:");
-    assert.equal(user[1],_contactNo,"Contact No checked:");
-    assert.equal(user[2],_role,"Role checked:");
-    assert.equal(user[3],_isActive,"isActive checked:");
-    assert.equal(user[4],_profileHash,"Profile Hash checked:");
-    assert.isTrue(true);
-
-    callback(true);
-}
